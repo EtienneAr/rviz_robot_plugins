a5d2ccbcd6e8b8f020dc1659ac45abf6476ecf95
earlaud <etienne.arlaud@inria.fr>
2025-07-08 15:50:58 +0200

    Add robot_description topic selection property

--- include/rviz_robot_plugins/joint_state_robot_display.hpp
+++ include/rviz_robot_plugins/joint_state_robot_display.hpp
@@ -2,6 +2,9 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <rviz_common/display.hpp>
+#include <rviz_common/display_context.hpp>
+#include "rviz_common/properties/ros_topic_property.hpp"
+#include "rviz_common/ros_integration/ros_node_abstraction_iface.hpp"
 
 namespace rviz_robot_plugins
 {
@@ -11,13 +14,20 @@ class JointStateRobotDisplay: public rviz_common::Display
   Q_OBJECT
 
 public:
-  JointStateRobotDisplay() {};
+  JointStateRobotDisplay();
   virtual ~JointStateRobotDisplay() override = default;
 
 protected:
-  void onInitialize() override {};
+  void onInitialize() override;
   void onEnable() override {};
   void onDisable() override {};
+
+protected Q_SLOTS:
+  void updateRobotDescriptionTopic();
+
+private:
+  rviz_common::ros_integration::RosNodeAbstractionIface::WeakPtr rviz_ros_node_;
+  rviz_common::properties::RosTopicProperty * topic_property_;
 };
 
 }  // namespace rviz_robot_plugins
--- src/joint_state_robot_display.cpp
+++ src/joint_state_robot_display.cpp
@@ -1,4 +1,35 @@
 #include <pluginlib/class_list_macros.hpp>
+#include <rosidl_runtime_cpp/traits.hpp>
+#include <std_msgs/msg/string.hpp>
+
 #include "rviz_robot_plugins/joint_state_robot_display.hpp"
+
+namespace rviz_robot_plugins
+{
+
+JointStateRobotDisplay::JointStateRobotDisplay()
+{
+    topic_property_ = new rviz_common::properties::RosTopicProperty(
+        "Robot Description",
+        "/robot_description",
+        QString::fromStdString(rosidl_generator_traits::name<std_msgs::msg::String>()),
+        "Topic to get robot urdf model",
+        this,
+        SLOT(updateRobotDescriptionTopic())
+    );
+}
+
+void JointStateRobotDisplay::onInitialize()
+{
+    rviz_ros_node_ = context_->getRosNodeAbstraction();
+    topic_property_->initialize(rviz_ros_node_);
+}
+
+void JointStateRobotDisplay::updateRobotDescriptionTopic()
+{
+    context_->queueRender();
+}
+
+} //namespace rviz_robot_plugins
 
 PLUGINLIB_EXPORT_CLASS(rviz_robot_plugins::JointStateRobotDisplay, rviz_common::Display)
